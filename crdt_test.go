533efb8d7f262d2d42ebed4e23a26d430d0c71c2
Austin Mason <amason@evernote.com>
2021-07-07 11:14:14 -0700

    fix indent

--- crdt_test.go
+++ crdt_test.go
@@ -13,6 +13,6 @@ func ExampleTree() {
 	tree := Tree{ID: "abc", Root: node}
 	fmt.Println(tree.String())
 	// Output:
-  // You are creating a CRDTree
+	// You are creating a CRDTree
 	// ID: abc, Structure: Node[ID: root, Value: Value: string value, Parent: none, Children: [Node[ID: child1, Value: Value: string value, Parent: root, Children: []]]]
 }
